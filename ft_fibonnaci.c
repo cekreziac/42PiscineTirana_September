3c92ded48ce669cf23e0e45e0265413fdc5a00bb
cekreziac <cekrezi.ac@gmail.com>
2025-09-06 16:37:19 -0400

    Fibonacci

---
+++ ft_fibonnaci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int ft_fibonacci(int index)
+{
+    if(index < 0)
+        return (-1);
+    else if(index == 0)
+        return (0);
+    else if(index == 1)
+        return (1);
+    return (ft_fibonacci(index -1) + ft_fibonacci(index -2));
+}
+
+int main()
+{
+    printf("%d", ft_fibonacci(-1));
+    printf("\n");
+    printf("%d", ft_fibonacci(0));
+    printf("\n");
+    printf("%d", ft_fibonacci(7));
+    printf("\n");
+    return (0);
+}
